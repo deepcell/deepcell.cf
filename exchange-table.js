724c0f4102cd36c50a7a14d2eb3525562a1fd7bd
deepcell <deepcell@gmail.com>
2019-02-21 14:05:41 +0700

    ..

--- exchange-table.js
+++ exchange-table.js
@@ -1,3 +1,7 @@
 $().ready(function() {
    $("#show-data").html("Text added by jQuery code.");
 });
+$.get('https://openexchangerates.org/api/latest.json', {app_id: '04ef44710d2a426b9fe6b899b8ee4eeb', show_alternative: 1}, function(data) {
+    console.log(data);
+    alert(data);
+});
